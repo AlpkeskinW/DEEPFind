365fad26143e96d49d9453e0367d28deff1b266b
AlpkeskinW <130823029+AlpkeskinW@users.noreply.github.com>
2023-04-16 15:35:06 +0300

    Add files via upload

---
+++ deepfindOSdosapp.py
@@ -0,0 +1,19 @@
+print("Currently running deepfind Linux distribution chooser Beta-1.0.")
+print("WARNING: IF YOU INSTALLED ONE OF THESE OPERATING SYSTEMS AND YOUR PC COULDN'T HANDLE IT, IT IS NOT OUR PROBLEM.")
+quest1 = input("Which activities do you do the most? Type the number. 1: Surfing on the internet 2: Coding  3: Hosting 4: Penetration Testing")
+quest2 = input("How would you describe your experience level? Type the number. 1: Beginner 2: Intermediate 3: Advanced 4: Wise")
+if quest1 = "1":
+    print("The recommended linux distributions for you are: Arch linux, Ubuntu linux, Pardus, Lubuntu, Linux LITE")
+
+if quest1 = "2", quest2 = "3":
+     print("The recommended linux distributions for you are: Arch linux, Ubuntu linux, Pardus, Lubuntu, Linux LITE, Kali Linux, BlackArch, Parrot Home")
+
+if quest1 = "2", quest2 = "4":
+     print("The recommended linux distributions for you are: Arch linux, Ubuntu linux, Pardus, Lubuntu, Linux LITE, Kali Linux, BlackArch, Parrot Home, Parrot Architect")
+
+if quest1 = "3":
+     print("The recommended linux distributions for you are: Arch linux, Ubuntu, Ubuntu Server, ")
+
+if quest1 = "4"
+     print("The recommended linux distributions for you are: Kali linux, Parrot Security OS, Parrot HTB, BlackArch Linux ")
+    
